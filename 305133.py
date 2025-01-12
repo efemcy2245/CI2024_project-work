af30e7803d2714ee38c178f3651d740ee05b2012
efemcy2245 <133202060+efemcy2245@users.noreply.github.com>
2025-01-12 11:44:24 +0100

    Create 305133.py

---
+++ 305133.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def f1(x):
+    return 
+
+
+def f2(x):
+    return 
+
+
+def f3(x):
+    return 
+
+
+def f4(x):
+    return 
+
+
+def f5(x):
+    return 
+
+
+def f6(x):
+    return 
+
+
+def f7(x):
+    return 
+
+
+def f8(x):
+    return 
